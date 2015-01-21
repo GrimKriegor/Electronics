4914016191965054421e27866c86c104e0b9f79c
GrimKriegor <grimkriegor@gmail.com>
2015-01-21 05:18:42 +0000

    Added option to ajust intensity via analog input.

--- CandleFlame.ino
+++ CandleFlame.ino
@@ -2,8 +2,13 @@
 
 
 //Led pin list
-int CANDLES[] = {3, 5, 6};
+int CANDLES[] = {9, 10, 11};
 int CANDLE_COUNT = sizeof(CANDLES) / sizeof(int);
+
+//Configure Intensity, or tune it via analog input
+int INTENSITY_MAX = 100;
+int INTENSITY_MIN = 50;
+boolean INTENSITY_AJUST = true; //Set to true to set the intensity via analog input
 
 //Sets LED pins in CANDLES[] to OUTPUT
 void setup()
@@ -16,8 +21,12 @@ void setup()
 //Changes intensity of each LED
 void loop()
 {
+	if (INTENSITY_AJUST) { 
+		INTENSITY_MAX = map(analogRead(0), 0, 670, 5, 100); 
+		INTENSITY_MIN = map(analogRead(0), 0, 670, 2, 50); }
+	
 	for (int LED; LED < CANDLE_COUNT; LED++) {
-		analogWrite(CANDLES[LED], random(50,100)); 
+		analogWrite(CANDLES[LED], random(INTENSITY_MIN, INTENSITY_MAX)); 
 	}
 	delay(random(80,150));
 }
