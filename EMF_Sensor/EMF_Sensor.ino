9544611d94f5cbff7c14cdb81fd9838444d3d666
GrimKriegor <grimkriegor@gmail.com>
2015-05-10 00:52:41 +0100

    Added project documentation.

--- EMF_Sensor/EMF_Sensor.ino
+++ EMF_Sensor/EMF_Sensor.ino
@@ -1,15 +1,26 @@
+//
 // :: EMF Sensor ::
+//
 // Original concept by Aaron ALAI - http://www.aaronalai.com/emf-detector
+// Grim Kriegor <grimkriegor@opmbx.org>
+//
+// Circuit suggestion:
+// * 6 LED connected to {12,10,8,6,4,2} digital pins via 100 ohm resistors
+// * Probe connected to analog pin 7
+// * Probe connected to ground via 3 Mohm resistor
+// 
 
+
+//Configuration
 int NUMREADINGS = 15;
 int SENSITIVITY = 15;
 byte PROBE_PIN   = A7;
-byte LEDS[] = {12,10,8,6,4,2};
+byte LEDS[] = {12,10,8,6,4,2}; // Replace with LED digital pins
 
+
+//Initialization
 byte LEDS_COUNT = sizeof(LEDS) / sizeof(byte);
 int VALUE = 0;
-
-//smoothing
 int READINGS[15];
 int INDEX = 0;
 int TOTAL = 0;
@@ -18,32 +29,48 @@ int LEVEL = 0;
 
 
 void setup() {
-  for (int i = 0; i < NUMREADINGS; i++) { READINGS[i] = 0; }
-
-  for (int i = 0; i < LEDS_COUNT; i++) { pinMode(LEDS[i], OUTPUT); }
-  for (int i = 0; i < LEDS_COUNT; i++) { digitalWrite(LEDS[i], HIGH); delay(100); digitalWrite(LEDS[i], LOW); }
   
+  //Clean READINGS array
+  for (int i = 0; i < NUMREADINGS; i++) { READINGS[i] = 0; }
+  
+  //Set all LED pins to OUTPUT
+  for (int i = 0; i < LEDS_COUNT; i++) { pinMode(LEDS[i], OUTPUT); }
+  
+  //Initialization LED Sequence
+  for (int i = 0; i < LEDS_COUNT; i++) { digitalWrite(LEDS[i], HIGH); delay(100); digitalWrite(LEDS[i], LOW); }
+  for (int i = LEDS_COUNT; i > 0; i--) { digitalWrite(LEDS[i], HIGH); delay(100); digitalWrite(LEDS[i], LOW); }
+
+  //Initialize Serial for debugging
   Serial.begin(9600);
 }
 
 void loop() {
-  VALUE = analogRead(PROBE_PIN); // Take reading
   
+  //Analog read from PROBE_PIN
+  VALUE = analogRead(PROBE_PIN);
+  
+  //Only compute if VALUE is 1 or greater
   if (VALUE >=1) {
+    
+    //Limits VALUE between 1 and SENSITIVITY then transforms it in an integer between 1 and 1023
     VALUE = constrain(VALUE, 1, SENSITIVITY);
     VALUE = map(VALUE, 1, SENSITIVITY, 1, 1023);
     
+    //Append new reading to READINGS array
     TOTAL -= READINGS[INDEX];
     READINGS[INDEX] = VALUE;
     TOTAL += READINGS[INDEX];
     INDEX++;
     
+    //Restart readings if INDEX >= maximum NUMREADINGS
     if (INDEX >= NUMREADINGS) { INDEX = 0; };
     
     AVERAGE = TOTAL / NUMREADINGS;
     
-    LEVEL = map(AVERAGE, 1, 1023, 0, 6);
+    //Converts AVERAGE to a number between 0 and the maximum number of LEDS (LEDS_COUNT)
+    LEVEL = map(AVERAGE, 1, 1023, 0, LEDS_COUNT);
     
+    //Every iteration, turn on LEVEL number LEDs
     for (int i = 0; i < LEVEL; i++) { digitalWrite(LEDS[i], HIGH); }
     for (int i = 0; i < LEDS_COUNT; i++) { digitalWrite(LEDS[i], LOW); }
 
